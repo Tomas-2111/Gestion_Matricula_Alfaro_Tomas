adbfd40236e15251e7b53bb95d8db5b82b17535d
Tomas-2111 <tojoalgu@gmail.com>
2026-07-12 22:38:44 -0600

    Cambio en index

--- GestionMatricula/Controllers/HomeController.cs
+++ GestionMatricula/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using GestionMatricula.Data;
 using GestionMatricula.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GestionMatricula.Controllers
@@ -7,15 +9,20 @@ namespace GestionMatricula.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
+        
+      
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Carreras.ToListAsync());
         }
 
         public IActionResult Privacy()
